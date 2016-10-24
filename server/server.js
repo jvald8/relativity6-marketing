049a5b42f32f8acb2acc20529807529365da672b
Jon Ringvald <jvald8@gmail.com>
2016-10-23 22:09:23 -0700

    first commit

---
+++ server/server.js
@@ -0,0 +1,46 @@
+var express = require('express'),
+	app = express(),
+	router = express.Router(),
+	bodyParser = require('body-parser'),
+	fs = require('fs'),
+	path = require('path'),
+	_ = require('lodash');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+// different views for different reasons
+// home view
+app.get('/', function(req, res, next) {
+	res.render('home', {});
+});
+
+app.get('/demo', function(req, res, next) {
+	res.render('demo', {});
+});
+
+router.use(function(req, res, next) {
+	console.log('something is happening');
+	next();
+});
+
+app.use(express.static('public'));
+
+// these are the api, to submit forms with
+app.use('/api', router);
+
+app.set('views' ,'./public/views');
+
+app.set('view engine', 'pug');
+
+app.listen(port);
+console.log(`go to localhost: ${port}`);
+
