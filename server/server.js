8f438946cc8fb8a997db5b1131152b5bf0cd2338
Jon Ringvald <jvald8@gmail.com>
2016-10-25 13:05:21 -0700

    try taking off the header xxs

--- server/server.js
+++ server/server.js
@@ -11,11 +11,11 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
 
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-});
+});*/
 
 // different views for different reasons
 // home view
