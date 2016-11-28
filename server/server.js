47d2a241fc60cb6ce8441ad5f25772b2754e7485
Jon Ringvald <jvald8@gmail.com>
2016-11-27 22:45:41 -0800

    has to be a heroku thing

--- server/server.js
+++ server/server.js
@@ -66,9 +66,9 @@ app.get('/contact-result', function(req, res, next) {
 
 app.get('/blog/*', function(req, res, next) {
   var newUrl = "http://blog.relativity6.com/blog" + req.url;
-  //res.redirect(newUrl, 301);
+  res.redirect(newUrl, 302);
   //next();
-  res.send(newUrl)
+  //res.send(newUrl)
 });
 
 router.use(function(req, res, next) {
