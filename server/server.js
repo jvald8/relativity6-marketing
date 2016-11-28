da127a23469b54eb2350742c57b14fc13ee6f013
Jon Ringvald <jvald8@gmail.com>
2016-11-27 22:51:06 -0800

    separate out blog

--- server/server.js
+++ server/server.js
@@ -65,7 +65,7 @@ app.get('/contact-result', function(req, res, next) {
 });
 
 app.get('/blog/*', function(req, res, next) {
-  var newUrl = "http://blog.relativity6.com/blog" + req.url;
+  var newUrl = "http://blog.relativity6.com/" + "blog" + + req.url;
   res.redirect(newUrl, 302);
   //next();
   //res.send(newUrl)
